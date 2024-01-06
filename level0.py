158695c8fa2b1ae834a81ff32fdff8793923869a
Abinaya-V-1008 <21pd01@psgtech.ac.in>
2024-01-06 14:32:43 +0530

    final

--- level0.py
+++ level0.py
@@ -5,8 +5,7 @@ with open('level0.json','r') as f:
     data=json.load(f)
 
 cost.append([0]+ data['restaurants']['r0']['neighbourhood_distance'])
-cost.append([data['restaurants']['r0']['neighbourhood_distance'][0]]+data['neighbourhoods']['n0']['distances'])
-#print(cost)
+cost.append([data['restaurants']['r0']['neighbourhood_distance'][0]] + data['neighbourhoods']['n0']['distances'])
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][1]] + data['neighbourhoods']['n1']['distances'])
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][2]] + data['neighbourhoods']['n2']['distances'])
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][3]] + data['neighbourhoods']['n3']['distances'])
@@ -26,15 +25,10 @@ cost.append([data['restaurants']['r0']['neighbourhood_distance'][16]] + data['ne
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][17]] + data['neighbourhoods']['n17']['distances'])
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][18]] + data['neighbourhoods']['n18']['distances'])
 cost.append([data['restaurants']['r0']['neighbourhood_distance'][19]] + data['neighbourhoods']['n19']['distances'])
-#cost.append(data['restaurants']['r0']['neighbourhood_distance'])
 
 
 print(cost)
 print(len(cost))
-#import numpy as np
-#cost=np.array(cost)
-#print(cost.shape)
-
 
 import math
 def solve_tsp_nearest(distances):
@@ -43,18 +37,15 @@ def solve_tsp_nearest(distances):
     tour = []
     total_distance = 0
     
-    # Start at the first city
     current_city = 0
     tour.append(current_city)
     visited[current_city] = True
     
     
-    # Repeat until all cities have been visited
     while len(tour) < num_cities:
         nearest_city = None
         nearest_distance = math.inf
 
-        # Find the nearest unvisited city
         for city in range(num_cities):
             if not visited[city]:
                 distance = distances[current_city][city]
@@ -62,13 +53,11 @@ def solve_tsp_nearest(distances):
                     nearest_city = city
                     nearest_distance = distance
 
-        # Move to the nearest city
         current_city = nearest_city
         tour.append(current_city)
         visited[current_city] = True
         total_distance += nearest_distance
 
-    # Complete the tour by returning to the starting city
     tour.append(0)
     total_distance += distances[current_city][0]
 
@@ -77,17 +66,20 @@ def solve_tsp_nearest(distances):
 tour, total_distance = solve_tsp_nearest(cost)
 
 print("Tour:", tour)
+
 rd=min(data['restaurants']['r0']['neighbourhood_distance'])
 print(rd)
 print("Total distance:", total_distance + 2*rd)
 
-my_dict = {'ro': 0}
+my_dict = {'r0': 0}
 
-for i in range(1, 21):
+for i in range(0,21):
     key = f'n{i}'
-    my_dict[key] = i
+    my_dict[key] = i+1
 
 print(my_dict)
+
+
 reverse_dict = {value: key for key, value in my_dict.items()}
 mapped_keys = [reverse_dict[value] for value in tour]
 print(mapped_keys)
